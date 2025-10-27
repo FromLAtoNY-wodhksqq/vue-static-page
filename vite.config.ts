095f6000aafe7f1637a01b931b6b03338e6eeada
FromLAtoNY-wodhksqq <wodhksqq@gmail.com>
2025-10-28 01:06:07 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-    base: '/vue-static-page/' 1
+    base: '/vue-static-page/'
 })
